9cba6ce9ad5892592387b53ef9adf0337eaddda6
wardxela <wardxela@gmail.com>
2025-11-23 19:51:26 +0300

    chore: update dependencies

--- home/shared/nix-search-tv.nix
+++ home/shared/nix-search-tv.nix
@@ -19,7 +19,7 @@ let
   ns-script =
     {
       url = "https://raw.githubusercontent.com/3timeslazy/nix-search-tv/refs/heads/main/nixpkgs.sh";
-      hash = "sha256-bEiwM2i9+AKpUuWV3D4y+EIm5AxArczz8Dpmf0R8svw=";
+      hash = "sha256-XkBL7EdPIETdi8B5k0ww3d66xB7QnW+mFEK2RUihWcY=";
     }
     |> pkgs.fetchurl
     |> builtins.readFile
