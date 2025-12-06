069b6b1772b1215e6dae007ff10df5c4a05c8dcb
wardxela <wardxela@gmail.com>
2025-12-07 02:24:24 +0300

    feat(noctalia): improve colorscheme

--- home/linux/noctalia.nix
+++ home/linux/noctalia.nix
@@ -9,21 +9,23 @@
     enable = true;
     systemd.enable = true;
     # TODO: ideally it should be implemented on stylix side
-    colors = with config.lib.stylix.colors; {
-      mError = "#${base08}";
-      mOnError = "#${base00}";
-      mOnPrimary = "#${base00}";
-      mOnSecondary = "#${base00}";
-      mOnSurface = "#${base04}";
-      mOnSurfaceVariant = "#${base04}";
-      mOnTertiary = "#${base00}";
-      mOutline = "#${base02}";
-      mPrimary = "#${base0B}";
-      mSecondary = "#${base0A}";
-      mShadow = "#${base00}";
-      mSurface = "#${base00}";
-      mSurfaceVariant = "#${base01}";
-      mTertiary = "#${base0D}";
+    colors = with config.lib.stylix.colors.withHashtag; {
+      mPrimary = base05;
+      mOnPrimary = base00;
+      mSecondary = base05;
+      mOnSecondary = base00;
+      mTertiary = base04;
+      mOnTertiary = base00;
+      mError = base08;
+      mOnError = base00;
+      mSurface = base00;
+      mOnSurface = base05;
+      mHover = base04;
+      mOnHover = base00;
+      mSurfaceVariant = base01;
+      mOnSurfaceVariant = base04;
+      mOutline = base02;
+      mShadow = base00;
     };
   };
 }
