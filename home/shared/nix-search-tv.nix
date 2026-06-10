fe4665cc9fa650d3fcac96fd9dd6ffc284a2265a
wardxela <wardxela@gmail.com>
2026-06-11 02:45:11 +0300

    Remove nix-search-tv script. Use tv integration

--- home/shared/nix-search-tv.nix
+++ home/shared/nix-search-tv.nix
@@ -1,19 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 
-let
-  ns-script =
-    {
-      url = "https://raw.githubusercontent.com/3timeslazy/nix-search-tv/refs/heads/main/nixpkgs.sh";
-      hash = "sha256-Kz1L8S0OCK9i9h79T3ISt9AO23LhT3hZAnNaIyhFYjE=";
-    }
-    |> pkgs.fetchurl
-    |> builtins.readFile
-    |> pkgs.writeShellScriptBin "ns";
-in
 {
   programs.nix-search-tv = {
     enable = true;
   };
-
-  home.packages = [ ns-script ];
 }
