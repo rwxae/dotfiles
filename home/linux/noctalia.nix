026dfa1a294a9a435b881389ecdd99776cc7f12c
wardxela <wardxela@gmail.com>
2026-07-05 20:38:35 +0300

    noctalia: remove settings

--- home/linux/noctalia.nix
+++ home/linux/noctalia.nix
@@ -1,8 +1,4 @@
-{
-  inputs,
-  config,
-  ...
-}:
+{ inputs, ... }:
 
 {
   imports = [
@@ -12,82 +8,5 @@
   programs.noctalia = {
     enable = true;
     systemd.enable = true;
-    settings =
-      let
-        monitors = [ config.mySystem.monitors.primary ];
-      in
-      {
-        bar = {
-          inherit monitors;
-          outerCorners = false;
-          widgets = {
-            left = [
-              {
-                id = "SystemMonitor";
-                showDiskUsage = true;
-                showNetworkStats = true;
-              }
-            ];
-            center = [
-              {
-                id = "MediaMini";
-                maxWidth = 300;
-                showAlbumArt = true;
-              }
-            ];
-            right = [
-              {
-                id = "Tray";
-                colorizeIcons = true;
-                drawerEnabled = false;
-              }
-              {
-                id = "KeyboardLayout";
-                displayMode = "forceOpen";
-              }
-              {
-                id = "Volume";
-                displayMode = "alwaysShow";
-              }
-              {
-                id = "Bluetooth";
-              }
-              {
-                id = "NotificationHistory";
-              }
-              {
-                id = "Clock";
-                formatHorizontal = "h:mm AP";
-                formatVertical = "h:mm AP";
-              }
-              {
-                id = "ControlCenter";
-                useDistroLogo = true;
-                colorizeSystemIcon = "primary";
-                enableColorization = true;
-              }
-            ];
-          };
-        };
-        general = {
-          animationDisabled = true;
-          enableShadows = false;
-          dimmerOpacity = 0;
-        };
-        ui = {
-          boxBorderEnabled = true;
-        };
-        dock.enabled = false;
-        notifications = {
-          inherit monitors;
-        };
-        osd = {
-          inherit monitors;
-          location = "bottom";
-        };
-        colorSchemes = {
-          generateTemplatesForPredefined = false;
-        };
-      };
   };
 }
