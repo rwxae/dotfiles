bcfbfb7471f32c87db3e5e151245f0efc8ab2209
wardxela <wardxela@gmail.com>
2026-07-08 16:10:29 +0300

    vicinae: change settings

--- home/linux/vicinae.nix
+++ home/linux/vicinae.nix
@@ -4,5 +4,13 @@
   programs.vicinae = {
     enable = true;
     systemd.enable = true;
+    settings = {
+      launcher_window = {
+        compact_mode.enabled = true;
+      };
+      keybinding = "emacs";
+      pop_on_backspace = false;
+      pop_to_root_on_close = true;
+    };
   };
 }
