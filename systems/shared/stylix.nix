c6abd301cabfe71b2fef8c677d17242a6905c513
wardxela <wardxela@gmail.com>
2026-07-20 17:37:38 +0300

    stylix: change theme

--- systems/shared/stylix.nix
+++ systems/shared/stylix.nix
@@ -3,7 +3,7 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/purpledream.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/evenok-dark.yaml";
     fonts = with pkgs; {
       serif = {
         package = source-serif;
