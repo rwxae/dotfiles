0fffd533bfe5cdd6884e1fccfca4d87353b77ca1
wardxela <wardxela@gmail.com>
2026-07-28 13:44:23 +0300

    Add sing-box service

--- systems/linux/services/default.nix
+++ systems/linux/services/default.nix
@@ -7,6 +7,7 @@
     ./locate.nix
     ./openssh.nix
     ./pipewire.nix
+    ./sing-box.nix
     ./udev.nix
   ];
 }
---
+++ systems/linux/services/sing-box.nix
@@ -0,0 +1,88 @@
+{ ... }:
+
+{
+  services.sing-box = {
+    enable = true;
+    settings = {
+      dns = {
+        strategy = "ipv4_only";
+        reverse_mapping = true;
+        servers = [
+          {
+            type = "https";
+            tag = "dns-global";
+            server = "8.8.8.8";
+            tls.server_name = "dns.google";
+          }
+        ];
+      };
+      inbounds = [
+        {
+          type = "tun";
+          address = [ "172.19.0.1/30" ];
+          auto_route = true;
+          auto_redirect = true;
+          strict_route = true;
+          stack = "system";
+        }
+      ];
+      outbounds = [
+        {
+          type = "hysteria2";
+          tag = "proxy";
+          server = {
+            _secret = "/run/secrets/hysteria/server";
+          };
+          server_port = {
+            _secret = "/run/secrets/hysteria/server_port";
+            quote = false;
+          };
+          password = {
+            _secret = "/run/secrets/hysteria/auth";
+          };
+          tls = {
+            enabled = true;
+            server_name = {
+              _secret = "/run/secrets/hysteria/server_name";
+            };
+          };
+        }
+        {
+          type = "direct";
+          tag = "direct";
+        }
+      ];
+      route = {
+        auto_detect_interface = true;
+        rule_set = [
+          {
+            type = "remote";
+            tag = "category-ru";
+            format = "binary";
+            url = "https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-category-ru.srs";
+          }
+        ];
+        rules = [
+          {
+            action = "sniff";
+          }
+          {
+            protocol = "dns";
+            action = "hijack-dns";
+          }
+          {
+            action = "resolve";
+          }
+          {
+            ip_is_private = true;
+            outbound = "direct";
+          }
+          {
+            rule_set = [ "category-ru" ];
+            outbound = "direct";
+          }
+        ];
+      };
+    };
+  };
+}
