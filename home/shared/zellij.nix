b74499dd767e38c016580337cb9a6b7f3293c690
wardxela <wardxela@gmail.com>
2026-08-23 00:50:55 +0300

    zellij: update configuration
    - Enable shell integration
    - Exit shell on zellij exit
    - Unbind some of the most essential keybinds
    - Disable session serialization
    - Disable startup tips
    - Add autolock plugin

--- home/shared/zellij.nix
+++ home/shared/zellij.nix
@@ -1,7 +1,37 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.zellij = {
     enable = true;
+    enableZshIntegration = true;
+    attachExistingSession = true;
+    exitShellOnExit = true;
+    settings = {
+      keybinds = {
+        unbind._args = [
+          "Alt f"
+          "Ctrl b"
+          "Ctrl n"
+          "Ctrl p"
+        ];
+      };
+      session_serialization = false;
+      show_startup_tips = false;
+      plugins = {
+        autolock = {
+          triggers = builtins.concatStringsSep "|" [
+            "fzf"
+            "git"
+            "gitui"
+            "hx"
+            "tv"
+            "yazi"
+          ];
+        };
+      };
+    };
+    plugins = with pkgs.zellijPlugins; [
+      autolock
+    ];
   };
 }
