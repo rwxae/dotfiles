ee0cce78f685a2e432e4e25b21c17e9bfa546dea
wardxela <wardxela@gmail.com>
2026-08-23 20:43:54 +0300

    zen-browser: remove Privacy Badger extension

--- home/shared/zen-browser.nix
+++ home/shared/zen-browser.nix
@@ -17,12 +17,6 @@
       DisablePocket = true;
       DisableTelemetry = true;
       OfferToSaveLogins = false;
-      ExtensionSettings = {
-        "jid1-MnnxcxisBPnSXQ@jetpack" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
-          installation_mode = "force_installed";
-        };
-      };
     };
     profiles.wardxela = { };
   };
