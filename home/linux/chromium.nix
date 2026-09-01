d83f8f3106dd9c51f9258a858e5fd1745ed75722
wardxela <wardxela@gmail.com>
2026-09-01 14:06:56 +0300

    chromium: update extensions

--- home/linux/chromium.nix
+++ home/linux/chromium.nix
@@ -10,18 +10,16 @@
       { id = "khncfooichmfjbepaaaebmommgaepoid"; }
       # SponsorBlock for YouTube - Skip Sponsorships
       { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; }
-      # Refined GitHub
-      { id = "hlepfoohegkhhmjieoechaddaejaokhf"; }
-      # Yang! - Yet Another Bangs anywhere extension
-      { id = "ecboojkidbdghfhifefbpdkdollfhicb"; }
       # Return YouTube Dislike
       { id = "gebbhagfogifgggkldgodflihgfeippi"; }
+      # Yang! - Yet Another Bangs anywhere extension
+      { id = "ecboojkidbdghfhifefbpdkdollfhicb"; }
       # Copy Current URL
       { id = "okkmnbabeggdmakmnffkoflpdlkmmpcp"; }
-      # Wappalyzer - Technology profiler
-      { id = "gppongmhjkpfnbhagpmjfkannfbllamg"; }
       # Simple Translate
       { id = "ibplnjkanclpjokhdolnendpplpjiace"; }
+      # DeArrow - Better Titles and Thumbnails
+      { id = "enamippconapkdmgfgjchkhakpfinmaj"; }
     ];
   };
 }
