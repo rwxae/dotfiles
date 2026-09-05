a4af515ae17719dcee90081f7b0441eb12f27e6f
wardxela <wardxela@gmail.com>
2026-09-05 16:29:36 +0300

    zellij: add room plugin and update main layout

--- home/shared/zellij.nix
+++ home/shared/zellij.nix
@@ -1,5 +1,31 @@
 { pkgs, ... }:
 
+let
+  wasmPkgs = pkgs.pkgsCross.wasm32-wasip1;
+  # TODO: Add this plugin in nixpkgs and use via zellijPlugins
+  zellij-room = wasmPkgs.rustPlatform.buildRustPackage (finalAttrs: {
+    pname = "zellij-room";
+    version = "1.2.1";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "rvcas";
+      repo = "room";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-KHCt4U0uqK/qkTGq2/Jf5bqBBhVDqAgFh80RPpU/KY0=";
+    };
+
+    nativeBuildInputs = [ wasmPkgs.lld ];
+
+    installPhase = ''
+      cp target/wasm32-wasip1/release/room.wasm $out
+    '';
+
+    env.RUSTFLAGS = "-C linker=wasm-ld";
+
+    cargoHash = "sha256-CtaMzE72YV/DPPhjxL6LCvA03J8MneSOnMdy4mkyXvE=";
+  });
+in
+
 {
   programs.zellij = {
     enable = true;
@@ -8,32 +34,37 @@
     exitShellOnExit = true;
     settings = {
       default_layout = "main";
+      default_mode = "locked";
       keybinds = {
-        unbind._args = [
-          "Alt f"
-          "Ctrl b"
-          "Ctrl n"
-          "Ctrl p"
-          "Ctrl q"
-        ];
-      };
-      session_serialization = false;
-      show_startup_tips = false;
-      plugins = {
-        autolock = {
-          triggers = builtins.concatStringsSep "|" [
-            "fzf"
-            "git"
-            "gitui"
-            "hx"
-            "tv"
-            "yazi"
+        shared = {
+          _children = [
+            {
+              bind = {
+                _args = [ "Ctrl ." ];
+                _children = [
+                  {
+                    LaunchOrFocusPlugin = {
+                      _args = [ "room" ];
+                      _children = [
+                        {
+                          floating = true;
+                          ignore_case = true;
+                          quick_jump = true;
+                        }
+                      ];
+                    };
+                  }
+                ];
+              };
+            }
           ];
         };
       };
+      session_serialization = false;
+      show_startup_tips = false;
     };
-    plugins = with pkgs.zellijPlugins; [
-      autolock
+    plugins = [
+      zellij-room
     ];
     layouts = {
       main.layout._children = [
@@ -60,12 +91,48 @@
             }
           ];
         }
-        { tab = { }; }
-        { tab = { }; }
-        { tab = { }; }
-        { tab = { }; }
-        { tab = { }; }
-        { tab = { }; }
+        {
+          tab = {
+            _props = {
+              name = "Root";
+            };
+          };
+        }
+        {
+          tab = {
+            _props = {
+              name = "Edit";
+            };
+          };
+        }
+        {
+          tab = {
+            _props = {
+              name = "Chat";
+            };
+          };
+        }
+        {
+          tab = {
+            _props = {
+              name = "sh#1";
+            };
+          };
+        }
+        {
+          tab = {
+            _props = {
+              name = "sh#2";
+            };
+          };
+        }
+        {
+          tab = {
+            _props = {
+              name = "sh#3";
+            };
+          };
+        }
       ];
     };
   };
