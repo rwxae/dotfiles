d85d1d2ca5daaeccb307352d6281760829ff443f
wardxela <wardxela@gmail.com>
2026-09-19 14:48:30 +0300

    beast: configure monitors

--- hosts/beast/home.nix
+++ hosts/beast/home.nix
@@ -10,23 +10,16 @@
   programs.btop.settings.shown_boxes = "proc cpu mem net gpu0";
 
   wayland.windowManager.hyprland.settings.monitor = [
-    # TODO:
-    # {
-    #   output = "CHANGE_ME";
-    #   mode = "2560x1440@180";
-    #   position = "0x0";
-    #   scale = 1;
-    # }
-    # {
-    #   output = "DP-4";
-    #   mode = "1920x1080@144";
-    #   position = "2560x180";
-    #   scale = 1;
-    # }
     {
       output = "DP-4";
+      mode = "2560x1440@180";
+      position = "1920x0";
+      scale = 1.25;
+    }
+    {
+      output = "DP-5";
       mode = "1920x1080@144";
-      position = "0x0";
+      position = "0x180";
       scale = 1;
     }
   ];
