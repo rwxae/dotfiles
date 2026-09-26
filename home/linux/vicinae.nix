e1e4383c3d09daf8a24365bfc666a4944c1d4392
wardxela <wardxela@gmail.com>
2026-09-26 19:17:53 +0300

    vicinae: update settings

--- home/linux/vicinae.nix
+++ home/linux/vicinae.nix
@@ -8,9 +8,10 @@
       launcher_window = {
         compact_mode.enabled = true;
       };
-      keybinding = "emacs";
       pop_on_backspace = false;
       pop_to_root_on_close = true;
+      telemetry.system_info = false;
+      tray.enabled = false;
     };
   };
 }
